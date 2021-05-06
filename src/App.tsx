adb1c5d8ef511c60a27df2aff86ab7c7576257bc
devludaley <daleyluis@gmail.com>
2021-05-06 17:39:06 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import * as React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <p>My react app</p>
+            <p>This is a list of array methods</p>
+        </>
+    );
+};
+
+export default App;
